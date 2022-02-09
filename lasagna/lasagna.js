96659203534c43240bfe69a2bb9417190d5a611a
Isaias Ogbe <ogbeisaias@gmail.com>
2022-02-09 11:34:54 -0500

    exported expected oven time

--- lasagna/lasagna.js
+++ lasagna/lasagna.js
@@ -35,7 +35,7 @@
  */
 const PREPARATION_MINUTES_PER_LAYER = 2;
 
-const EXPECTED_MINUTES_IN_OVEN = 40;
+export const EXPECTED_MINUTES_IN_OVEN = 40;
 /**
  * Determines the number of minutes the lasagna still needs to remain in the
  * oven to be properly prepared.
